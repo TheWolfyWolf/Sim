21e446c7dd1801fa48d17792db36ddcd3eb6970d
Ryan Woods <48329085+TheWolfyWolf@users.noreply.github.com>
2020-05-24 15:44:08 +0100

    The Good Stuff

--- Block.py
+++ Block.py
@@ -7,16 +7,40 @@ class entity:
         self.spawn = spawn
         self.size = size
         self.colour = colour
+        self.target = spawn
+        self.timeInfected = 0
 
-    def move(self):
-        x = random.randint(-3,3)
-        y = random.randint(-3,3)
-        self.spawn[0] += x
-        self.spawn[1] += y
+    def genTarget(self, canvasSize):
+        target = [0,0]
+        target[0] = random.randint(0, canvasSize-10)
+        target[1] = random.randint(0, canvasSize-10)
+        return target
+
+    def move(self, canvasSize):
+        x = 0
+        y = 0
+        if self.spawn[0] == self.target[0] and self.spawn[1] == self.target[1]:
+            self.target = self.genTarget(canvasSize)
+        if self.spawn[0] > self.target[0]:
+            x = -1
+            self.spawn[0] += -1
+        if self.spawn[0] < self.target[0]:
+            x = 1
+            self.spawn[0] += 1
+        if self.spawn[1] > self.target[1]:
+            y = -1
+            self.spawn[1] += -1
+        if self.spawn[1] < self.target[1]:
+            y = 1
+            self.spawn[1] += 1
+
         return x,y
 
     def getSpawn(self):
         return self.spawn
+
+    def setTimeInfected(self):
+        self.timeInfected +=1
 
     def getSize(self):
         return self.size
--- GUI.py
+++ GUI.py
@@ -3,61 +3,94 @@ import Block as b
 from tkinter import *
 import time
 import random
-sec = 0.1
 root = Tk()
 
+"""Variables"""
+population = 1000
+sec = 0.1
+cavasSize = 1000
+
+""""""
+
+"""GUI"""
 class canvas:
     global Cav
-    Cav = Canvas(root, width=1000, height=1000)
-    Cav.pack()
+    frame = Frame(root, width=300, height=300)
+    frame.pack(expand=True, fill=BOTH)  # .grid(row=0,column=0)
+    Cav = Canvas(frame, bg='#FFFFFF', width=cavasSize, height=cavasSize, scrollregion=(0, 0, cavasSize, cavasSize))
+    hbar = Scrollbar(frame, orient=HORIZONTAL)
+    hbar.pack(side=BOTTOM, fill=X)
+    hbar.config(command=Cav.xview)
+    vbar = Scrollbar(frame, orient=VERTICAL)
+    vbar.pack(side=RIGHT, fill=Y)
+    vbar.config(command=Cav.yview)
+    Cav.config(width=300, height=300)
+    Cav.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+    Cav.pack(side=LEFT, expand=True, fill=BOTH)
 
     def update(self):
         root.update()
 
-    def moveBlock(self, block, x, y):
+    def moveBlock(self, block, x, y):`
         Cav.move(block, x, y)
 
-
-    ent = b.entity
-    rect =0
-
-
 C = canvas()
+Legends = StringVar()
+Legend = tkinter.Label(root, textvariable =Legends).pack()
 
+def updateLegend(pop,infected):
+    texty = "Population = ", pop, "  Active Infected =  ", infected
+    Legends.set(texty)
+    root.update_idletasks()
 
+""""""
+
+"""Generator"""
+ent = b.entity
+rect =0
 cubeys = []
-for i in range(0,1000):
-    if i == 1:
-        cubey = {"ent": b.entity([random.randint(50, 950), random.randint(50, 950)], 10, "RED")}
+badCubeys = []
+for i in range(0,population):
+    if i == 0:
+        cubey = {"ent": b.entity([random.randint(10, cavasSize-10), random.randint(10, cavasSize-10)], 10, "BLUE")}
         rect = Cav.create_rectangle(cubey["ent"].getSpawn()[0], cubey["ent"].getSpawn()[1],
                                     cubey["ent"].getSpawn()[0] + cubey["ent"].getSize(),
                                     cubey["ent"].getSpawn()[1] + cubey["ent"].getSize(), fill=cubey["ent"].getColour())
+        cubey["rect"] = rect
+        badCubeys.append(cubey)
+        print(cubey["ent"].getSpawn())
 
     else:
-        cubey = {"ent": b.entity([random.randint(50, 950), random.randint(50, 950)], 10, "GREEN")}
+        cubey = {"ent": b.entity([random.randint(10, cavasSize-10), random.randint(10, cavasSize-10)], 10, "GREEN")}
         rect = Cav.create_rectangle(cubey["ent"].getSpawn()[0], cubey["ent"].getSpawn()[1],
                                     cubey["ent"].getSpawn()[0] + cubey["ent"].getSize(),
                                     cubey["ent"].getSpawn()[1] + cubey["ent"].getSize(), fill=cubey["ent"].getColour())
-    cubey["rect"] = rect
+        cubey["rect"] = rect
     cubeys.append(cubey)
+""""""
 
-    C.update()
-
-print(cubeys)
-
+"""Main Loop"""
 while True:
     for X in cubeys:
-        x ,y = X["ent"].move()
+        x ,y = X["ent"].move(cavasSize)
         C.moveBlock(X["rect"], x, y)
 
-        if X["ent"].getColour() == "RED":
-            for I in cubeys:
-                if I["ent"].getColour() != "RED":
-                    if X["ent"].getSpawn()[0] >= I["ent"].getSpawn()[0]-10 and X["ent"].getSpawn()[0] <= I["ent"].getSpawn()[0] +10:
-                        if X["ent"].getSpawn()[1] >= I["ent"].getSpawn()[1]-10 and X["ent"].getSpawn()[1] <= I["ent"].getSpawn()[1] +10:
-                            I["ent"].setColour("RED")
-                            Cav.itemconfig(I["rect"], fill = "RED")
+        if X["ent"].getColour() == "RED" or X["ent"].getColour() == "BLUE":
+            X["ent"].setTimeInfected()
+        if X["ent"].timeInfected >= 10:
+            Cav.delete(X["rect"])
 
-    root.update()
+        if X["ent"].getColour() != "RED" and X["ent"].getColour() != "BLUE":
+            for I in badCubeys:
+                if I["ent"].getSpawn()[0] >= X["ent"].getSpawn()[0]-10 and I["ent"].getSpawn()[0] <= X["ent"].getSpawn()[0] +10 and I["ent"].getSpawn()[1] >= X["ent"].getSpawn()[1]-10 and I["ent"].getSpawn()[1] <= X["ent"].getSpawn()[1] +10:
+                        X["ent"].setColour("RED")
+                        Cav.itemconfig(X["rect"], fill = "RED")
+            if X["ent"].getColour() == "RED":
+                badCubeys.append(X)
 
-root.mainloop()
+    updateLegend(population, len(badCubeys))
+    C.update()
+""""""
+
+
+
